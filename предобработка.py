e13dd51711283818405aafe3590e2d7ce89fea15
sambrtheslayer <sambrtheslayer@mail.ru>
2019-11-14 17:13:15 +0500

    deleted unnecessary lines

--- предобработка.py
+++ предобработка.py
@@ -9,21 +9,15 @@ df = pd.read_csv('annotated_german.csv')
 df['content'] = df['content'].str.lower()
 #привели каждую строчку в столбце content к нижнему регистру
 
-df['content']
-
 df['content'] = df['content'].apply(lambda row: re.compile(r'(\W)\1+').sub(r'\1', str(row)))
 df['content'] = df['content'].apply(lambda row: re.compile(r'[^\w\s]').sub(r' ', str(row)).strip())
 #для каждой записи формируем регулярное выражение, с помощью которого уберем все знаки препинания,
 #и повторяющиеся символы
 #с помощью лямбда функций применяем это к каждой записи в столбце content
 
-df['content']
-
 df['content'] = df['content'].apply(lambda index: nltk.word_tokenize(index))
 #производим токенизацию(разбитие строк на смысловые единицы).
 #используем лямбда функцию, которая работает с каждой строкой content и возвращает список слов в эту строку
-
-df['content']
 
 df['content'] = df['content'].apply(lambda row: list(map(lambda index:SnowballStemmer(language='german').stem(index),row)))
 #далее производим стэмминг. Внешняя лямбда берет строку, а внутрення лямбда работает с каждым словом в этой строке
